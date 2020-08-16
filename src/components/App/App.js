fb4d8d883ba8aaa92d268d464f74ecab206a0129
Hans Accola <Hansolo6283@gmail.com>
2020-08-16 14:05:49 -0500

    changed font style/size in header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -47,7 +47,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
+          <h1 className="App-title">Hans Accola's Gallery</h1>
         </header>
         <br/>
         <GalleryList 
