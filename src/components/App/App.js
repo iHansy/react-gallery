9fe2507954251b12ca6b96535d36a29133deba0b
Hans Accola <Hansolo6283@gmail.com>
2020-08-16 11:12:51 -0500

    completed axios get request in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios'; //importing axios
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList.jsx'; //importing component
 
 class App extends Component {
+
+  state = {
+    galleryList: [] //empty array for list of photos to go in
+  }
+
+  //loading galleryList on first page load
+  componentDidMount() {
+    this.getGalleryList();
+  }
+
+  //axios request to get galleryList items from mock server
+  getGalleryList = () => {
+    //sending request to axios
+    axios.get('/gallery')
+    .then((response) => {
+      console.log(response.data); //testing to see data
+      //changing state
+      this.setState({
+        galleryList: response.data //where response data lives
+      })
+    })
+    .catch((error) => { //error from server
+      alert(`Couldn't get gallery list. Try again later.`);
+      console.log('Error getting gallery list', error);
+    })
+  }
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
